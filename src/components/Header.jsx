02577cd44b8cb16df96548ee66dea21ee3c3c9df
HenryRisco <lhrisco@gmail.com>
2024-03-20 18:38:10 +0100

    Crear proyecto de react con vite

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return <header className="border-b shadow py-2 px-3 flex justify-between items-center">
+        {/* Logo para version mobile */}
+        <img src="/cropped-favicon-192x192.png" alt="blockmaker-mobile-logo" className="sm:hidden" width={47} />
+        {/* Logo para version desktop */}
+        <img src="/logo-blockmaker.png" alt="blockmaker-desktop-logo" className="hidden sm:block" width={300} />
+        {/*<ConnectKitButton showBalance />*/}
+    </header>
+}
